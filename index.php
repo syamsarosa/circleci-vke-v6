121f33050e086a865abf95fab5a25904881798f8
Syam Sarosa <syamsarosa@gmail.com>
2025-12-15 16:20:57 +0000

    test auto deployment

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
- <!DOCTYPE html>
- <html>
- <head>
-   <title>Vultr Demo</title>
- </head>
- <body>
-   <h1><?php echo "Hello from Vultr"; ?></h1>
- </body>
- </html>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Vultr Demo</title>
+</head>
+<body>
+  <h1><?php echo "Hello from Vultr"; ?></h1>
+  <p>Soemthing</p>
+</body>
+</html>
